029889c660da9e49cea0c2fc61adbef231c3550d
Boris Bera <bera.boris@gmail.com>
2016-07-10 16:31:49 -0400

    Added basic arnold face toggle

--- src/pocket_arnold/core.cljs
+++ src/pocket_arnold/core.cljs
@@ -3,11 +3,16 @@
 
 (enable-console-print!)
 
-(defn hello []
-  [:p "Hello Pocket Arnold"])
+(defonce talking (r/atom false))
 
-(defn app-root []
-  [hello])
+(defn arnold-face [talking]
+  (let [path (if @talking "img/arnold-talking.png"
+                          "img/arnold-normal.png")]
+    [:img {:src path
+           :on-click #(swap! talking not)}]))
+
+(defn app-root [talking]
+  [:div [arnold-face talking]])
 
 (let [el (.getElementById js/document "app")]
-  (r/render-component [app-root] el))
+  (r/render-component [app-root talking] el))
