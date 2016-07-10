f47d9bb9a90a1c3abe4795a6cf758fa675837e98
Boris Bera <bera.boris@gmail.com>
2016-07-10 16:45:00 -0400

    Centered arnold and gave him alt + dim

--- src/pocket_arnold/core.cljs
+++ src/pocket_arnold/core.cljs
@@ -6,13 +6,20 @@
 (defonce talking (r/atom false))
 
 (defn arnold-face [talking]
-  (let [path (if @talking "img/arnold-talking.png"
-                          "img/arnold-normal.png")]
-    [:img {:src path
-           :on-click #(swap! talking not)}]))
+  (let [path (if @talking
+                 "img/arnold-talking.png"
+                 "img/arnold-normal.png")
+        alt (if @talking
+                "Arnold is talking"
+                "Arnold is not talking")]
+    [:div.face-ctn>img.face
+      {:src path
+       :alt alt
+       :width 360 :heigth 500
+       :on-click #(swap! talking not)}]))
 
 (defn app-root [talking]
-  [:div [arnold-face talking]])
+  [arnold-face talking])
 
 (let [el (.getElementById js/document "app")]
   (r/render-component [app-root talking] el))
