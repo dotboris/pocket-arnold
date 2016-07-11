374d40666724d8746e6d6bb9b9b35707f1c0b1f1
Boris Bera <bera.boris@gmail.com>
2016-07-10 20:53:49 -0400

    tip dissapears after first interaction

--- src/pocket_arnold/core.cljs
+++ src/pocket_arnold/core.cljs
@@ -4,10 +4,12 @@
 
 (enable-console-print!)
 
+(defonce show-tip (r/atom true))
 (defonce talking (r/atom false))
 (defonce quote (r/atom nil))
 
-(defn toggle-arnold! [talking quote]
+(defn toggle-arnold! [talking quote shpw-tip]
+  (reset! show-tip false)
   (swap! talking not)
   (when @talking
         (reset! quote (q/random-quote))))
@@ -18,7 +20,7 @@
       {:class (when-not @talking "hidden")}
       @quote]])
 
-(defn arnold-face [talking quote]
+(defn arnold-face [talking quote show-tip]
   (let [path (if @talking
                  "img/arnold-talking.png"
                  "img/arnold-normal.png")
@@ -28,13 +30,14 @@
     [:div.face-ctn>img.face
       {:src path
        :alt alt
-       :on-click #(toggle-arnold! talking quote)}]))
+       :on-click #(toggle-arnold! talking quote show-tip)}]))
 
-(defn app-root [talking quote]
+(defn app-root [talking quote show-tip]
   [:div.arnold
     [text-bubble talking quote]
-    [arnold-face talking quote]
-    [:div.tip "TIP: Click on Arnold"]])
+    [arnold-face talking quote show-tip]
+    (when @show-tip
+          [:div.tip "TIP: Click on Arnold"])])
 
 (let [el (.getElementById js/document "app")]
-  (r/render-component [app-root talking quote] el))
+  (r/render-component [app-root talking quote show-tip] el))
